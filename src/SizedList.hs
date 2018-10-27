39d2d7f8f82ba6904f522a6c0fbe529758445532
Daniel Bramucci <dbramuccicode@gmail.com>
2018-10-26 23:12:21 -0500

    Fixed some warnings from -Wall.

--- src/SizedList.hs
+++ src/SizedList.hs
@@ -5,8 +5,8 @@ module SizedList where
 data Nat = Zero | Suc Nat
 
 data SizedList a (n :: Nat) where
-    Nil :: SizedList a Zero
-    (:-:) :: a -> SizedList a m -> SizedList a (Suc m)
+    Nil :: SizedList a 'Zero
+    (:-:) :: a -> SizedList a m -> SizedList a ('Suc m)
 -- Make SizedList right associative with the same precedence as normal list consing.
 infixr 5 :-:
 
@@ -16,12 +16,12 @@ instance Show a => Show (SizedList a n) where
         where
             go :: SizedList a m -> String
             go Nil = ""
-            go (x :-: Nil) = show x
-            go (x :-: xs) = show x ++ ", " ++ go xs
+            go (y :-: Nil) = show y
+            go (y :-: ys) = show y ++ ", " ++ go ys
 
 
 -- | Gives the length of a SizedList
 -- This could be more type safe with the Singleton pattern but this is outside the scope of my current effort.
 len :: SizedList a m -> Int
 len Nil = 0
-len (x :-: xs) = 1 + len xs
+len (_ :-: xs) = 1 + len xs
